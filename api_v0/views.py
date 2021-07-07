3a9027afe00a3094466e2efaf7efdecdcf23f666
maximkovalev <makccom0@gmail.com>
2021-07-07 06:32:27 +0300

    qw

--- api_v0/views.py
+++ api_v0/views.py
@@ -854,7 +854,7 @@ class GetTests(APIView):
         df = pd.DataFrame(list_elements_table)
         df2 = pd.DataFrame(list_rating_users)
         df3 = pd.DataFrame(list_rating_team)
-        name = 'Отчет' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M") + '.xlsx'
+        name = 'Отчет.xlsx'
         writer = pd.ExcelWriter(MEDIA_ROOT + '/file_excel/' + name, engine='xlsxwriter')
         df.to_excel(writer, sheet_name='test')
         df2.to_excel(writer, sheet_name='rating_user')
