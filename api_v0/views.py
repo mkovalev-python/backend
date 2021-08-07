812d687b1d71be9b5d785f76a5bd6536a84a858c
mkovalev-python <78082300+mkovalev-python@users.noreply.github.com>
2021-08-07 15:16:38 +0300

    Update views.py

--- api_v0/views.py
+++ api_v0/views.py
@@ -802,7 +802,7 @@ class UploadUser(APIView):
                     ).save()
                     create_permission_for_user = PermissionUser(
                         permission_id='Participant',
-                        username_id=row['email'].split('@')[0]).save()
+                        username_id=row['email'].split('@')[0]+'1').save()
                     create_rating_field = Rating(username_id=row['email'].split('@')[0],
                                                  rating=Profile.objects.exclude(team='Staff').count(), points=0).save()
 
@@ -831,7 +831,7 @@ class UploadUser(APIView):
                             <p>Логин и пароль вы найдете ниже. Не откладывайте, переходите по ссылке сейчас.</p><br>
                              
                             
-                            <span><b>Login:</b>  """ + row['email'].split('@')[0] + """</span><br>
+                            <span><b>Login:</b>  """ + row['email'].split('@')[0] + '1' + """</span><br>
                             <span><b>Password:</b>  """ + password + """</span><br>
                             <a href='http://tspolls.ru/'>АВТОРИЗОВАТЬСЯ</a>
                             </body></html>"""
@@ -1220,7 +1220,7 @@ class SendNewPass(APIView):
                                     <p>Логин и пароль вы найдете ниже. Не откладывайте, переходите по ссылке сейчас.</p><br>
 
 
-                                    <span><b>Login:</b>  """ + get_email_user.email.split('@')[0] + """</span><br>
+                                    <span><b>Login:</b>  """ + get_email_user.email.split('@')[0]+ '1' + """</span><br>
                                     <span><b>Password:</b>  """ + password + """</span><br>
                                     <a href='http://tspolls.ru/'>АВТОРИЗОВАТЬСЯ</a>
                                     </body></html>"""
