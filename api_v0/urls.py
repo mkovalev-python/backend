f9ad514d56d914d61ad6e1210f342e3a527a21d9
maximkovalev <mkovalevhse@yandex.ru>
2021-09-22 22:34:36 +0300

    static url

--- api_v0/urls.py
+++ api_v0/urls.py
@@ -47,4 +47,3 @@ urlpatterns = [
     path('post/password/', PostPassword.as_view())
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
